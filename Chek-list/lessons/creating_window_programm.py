0a5abb30ba2cc3dd6098e59e6c4d60f9330d1b91
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-11-29 14:36:44 +0300

    r

--- Chek-list/lessons/creating_window_programm.py
+++ Chek-list/lessons/creating_window_programm.py
@@ -7,16 +7,18 @@ root = Tk()     #Переменная, которая содержит в себ
 """
 root = Tk()
 title_name = "Check-list (v.0)"
+label = Label(root, text="Дання программа является результатом прохождения бесплатного курса")
 
 screenwidth = root.winfo_screenwidth()      #Позволяет определить ширину экрана
 screenheight = root.winfo_screenheight()      #Позволяет определить ширину экрана
 screen_options = root.geometry().split('+')[0].split('x')       #Позволяет получить список параметров окна в формате [ширина, высота, координата X, координата Y]
 width_root = int(screen_options[0])     #Позволяет получить ширину окна
 height_root = int(screen_options[1])        #Позволяет получить высоту окна
-root.update_idletasks()     # Обновляем инфо об окне
 
+root.update_idletasks()     # Обновляем инфо об окне
 root.title(title_name)      #метод title() устанавливает заголовок окна
-    
+label.pack()
+
 
 root.geometry(f'{400}x{100}+{screenwidth // 2 - 200}+{screenheight // 2 - 50}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
         
