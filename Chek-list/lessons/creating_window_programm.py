7929272f7955194d10fb39842d4c35c944ad31f6
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-11-29 14:22:44 +0300

    ф

--- Chek-list/lessons/creating_window_programm.py
+++ Chek-list/lessons/creating_window_programm.py
@@ -5,36 +5,19 @@ root = Tk()     #Переменная, которая содержит в себ
 """
 Ниже добавляются необходимые компоненты
 """
+root = Tk()
+title_name = "Check-list (v.0)"
 
-def create_window(center, name):
+screenwidth = root.winfo_screenwidth()      #Позволяет определить ширину экрана
+screenheight = root.winfo_screenheight()      #Позволяет определить ширину экрана
+screen_options = root.geometry().split('+')[0].split('x')       #Позволяет получить список параметров окна в формате [ширина, высота, координата X, координата Y]
+width_root = int(screen_options[0])     #Позволяет получить ширину окна
+height_root = int(screen_options[1])        #Позволяет получить высоту окна
+root.update_idletasks()     # Обновляем инфо об окне
 
-    root = Tk()
+root.title(title_name)      #метод title() устанавливает заголовок окна
+    
 
-    screenwidth = root.winfo_screenwidth()      #Позволяет определить ширину экрана
-    screenheight = root.winfo_screenheight()      #Позволяет определить ширину экрана
-    screen_options = root.geometry().split('+')[0].split('x')       #Позволяет получить список параметров окна в формате [ширина, высота, координата X, координата Y]
-    width_root = int(screen_options[0])     #Позволяет получить ширину окна
-    height_root = int(screen_options[1])        #Позволяет получить высоту окна
-
-    root.title(name)      #метод title() устанавливает заголовок окна
-    match center:
-        case "center":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth // 2}+{screenheight // 2}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Top":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth // 2}+{0}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Bottom":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth // 2}+{screenheight}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Right":
-            root.geometry(f'{width_root}x{height_root}+{0}+{screenheight // 2}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Left":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth}+{screenheight // 2}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Top-right":
-            root.geometry(f'{width_root}x{height_root}+{0}+{0}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
-        case "Top-left":
-            root.geometry(f'{width_root}x{height_root}+{0}+{screenheight}')
-        case "Bottom-right":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth}+{0}')
-        case "Bottom-left":
-            root.geometry(f'{width_root}x{height_root}+{screenwidth}+{screenheight}')
-
-    root.mainloop()     #Создает бесконечный цикл, который отвечает за запуск программы
+root.geometry(f'{400}x{100}+{screenwidth // 2 - 200}+{screenheight // 2 - 50}')        #метод geometry() устанавливает размер окна (в аргументе "Ширина х Высота")
+        
+root.mainloop()     #Создает бесконечный цикл, который отвечает за запуск программы
